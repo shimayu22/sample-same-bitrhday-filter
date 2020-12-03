2fe30fa0894a4239cfd864faae307751092dab8a
Yutaka Shimada <shimayu22@gmail.com>
2020-12-03 15:31:44 +0900

    管理画面設定

--- sample/admin.py
+++ sample/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from sample.models import Samplepeople
+
+class SamplepeopleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'kana', 'date_of_birth')
+
+admin.site.register(Samplepeople, SamplepeopleAdmin)
