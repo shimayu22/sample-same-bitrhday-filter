af32a410773e7d7ee83057b4c6b0494bac62a024
Yutaka Shimada <shimayu22@gmail.com>
2020-12-03 13:50:16 +0900

    Modelを作成

--- sample/models.py
+++ sample/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Samplepeople(models.Model):
+    """仮のモデル"""
+
+    name = models.CharField(
+        verbose_name="名前",
+        max_length=5,
+    )
+
+    kana = models.CharField(
+        verbose_name="かな",
+        max_length=8,
+    )
+
+    date_of_birth = models.DateField(
+        verbose_name="生年月日"
+    )
