e775779f0c9e12a5127f14ce6a2eb593f1eef377
Yutaka Shimada <shimayu22@gmail.com>
2020-12-04 14:47:01 +0900

    add views

--- sampleapi/views.py
+++ sampleapi/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django_filters import rest_framework as filters
+from rest_framework import viewsets
 
-# Create your views here.
+from sample.models import Samplepeople
+from sampleapi.serializers import SamplepeopleSerializers
+
+class SampleFilter(filters.FilterSet):
+    birthday_month = filters.NumberFilter(field_name='date_of_birth', lookup_expr='month')
+    birthday_day = filters.NumberFilter(field_name='date_of_birth', lookup_expr='day')
+
+    class Meta:
+        model = Samplepeople
+        fields = ['id', 'name', 'kana', 'date_of_birth',]
+
+class SampleViewSet(viewsets.ReadOnlyModelViewSet):
+
+    queryset = Samplepeople.objects.all()
+    serializer_class = SamplepeopleSerializers
+    filter_backends = [filters.DjangoFilterBackend]
+    filter_class = SampleFilter
